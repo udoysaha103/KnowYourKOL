e74bfd9e722b71df5a0d1c3aba73240f952be502
captainfaisal <faisalhawlader04@gmail.com>
2025-04-01 02:51:02 +0600

    rmdir added

--- backend/controllers/deleteTempFiles.js
+++ backend/controllers/deleteTempFiles.js
@@ -1,8 +1,8 @@
-const { unlink } = require('fs').promises;
+const { rmdir } = require('fs').promises;
 const deleteTempFiles = async (path) => {
   // console.log(`Deleting temp files from ${path}`);
   try {
-    await unlink(path);
+    await rmdir(path, { recursive: true, force: true });
     console.log(`successfully deleted ${path}`);
   } catch (error) {
     console.error('there was an error:', error.message);
