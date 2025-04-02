1db49d053214c193502a00cf28a9cffdffadfc3a
captainfaisal <faisalhawlader04@gmail.com>
2025-04-02 17:21:47 +0600

    userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 // the basic schema
 const userSchema = new mongoose.Schema({
-    twitterId: { type: String, required: false, unique: true },
+    twitterId: { type: String, required: false },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: false },
