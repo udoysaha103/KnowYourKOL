afa683b3e36573a5549f4ea0a8501459d36d0926
captainfaisal <faisalhawlader04@gmail.com>
2025-04-12 07:34:50 +0600

    increase timeout for selector wait in scraper to 5 minutes

--- backend/controllers/scraper.js
+++ backend/controllers/scraper.js
@@ -73,7 +73,7 @@ const scrapData = async (accountAddress) => {
         // console.log(await page.content());
 
         // Wait for script tag to appear (adjust timeout if necessary)
-        await page.waitForSelector('script#__NEXT_DATA__', { timeout: 60000 });
+        await page.waitForSelector('script#__NEXT_DATA__', { timeout: 300000 });
 
         // Extract JSON data
         const jsonData = await page.evaluate(() => {
