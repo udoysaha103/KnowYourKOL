8cf79d6b5b690401564dfc314ef10c87a8172f9c
captainfaisal <faisalhawlader04@gmail.com>
2025-04-14 07:58:20 +0600

    fix: remove logout redirection to CLIENT_URL for improved logout handling

--- backend/routes/ssoAuth.js
+++ backend/routes/ssoAuth.js
@@ -7,7 +7,6 @@ const jwt = require("jsonwebtoken");
 router.get('/logout', (req, res, next) => {
     req.logout(err => {
         if (err) { return next(err); }
-        res.redirect(process.env.CLIENT_URL);
     });
 });
 
